56273cfd431f9dc2703d58f0e2aa977cb591acaa
gmmurray <trinilime123@live.com>
2023-01-25 17:58:26 -0500

    update static content

--- lib/staticContent.ts
+++ lib/staticContent.ts
@@ -1,13 +1,13 @@
 export const introStaticContent = {
     authorText: 'Greg Murray',
     authorImage:
-        'https://gregmurray.org/static/99a43881af925e4d65ca13dfbaf7fe3a/25252/professional_for_web.jpg.jpg',
+        'https://gregmurray.org/static/8841830f24b8838c515b4e46e71d8b6f/2f1b1/IMG_4396.JPG.jpg',
     webUrl: 'https://gregmurray.org',
     welcomeText: `
         Welcome to my blog! I'm Greg, a software engineer with a passion for learning and the human side
         of technology. This blog is a place for me to write about software development and a bit of life.
         I've learned so much through blogs and other content on the internet so hopefully my posts can
-        add a little bit to that.`,
+        add a little bit back to that.`,
 };
 
 export const postPageStaticContent = {
