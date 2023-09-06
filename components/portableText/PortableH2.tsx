1d506ca89bec4ebe2774c09e89115f2456f8d11f
gmmurray <trinilime123@live.com>
2023-09-05 20:41:29 -0400

    header links copy to clipboard

--- components/portableText/PortableH2.tsx
+++ components/portableText/PortableH2.tsx
@@ -1,31 +1,61 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Popover, Typography } from '@mui/material';
+import React, { useCallback, useState } from 'react';
 
 import { IconButton } from '@mui/material';
-import Link from 'next/link';
 import LinkIcon from '@mui/icons-material/Link';
-import React from 'react';
 import { toHyphenatedString } from '../../lib/stringHelpers';
 import { useRouter } from 'next/router';
 
 const PortableH2 = ({ children }: any) => {
     const router = useRouter();
-    if (!children || !Array.isArray(children) || children.length > 1) {
-        return <h2>{children}</h2>;
-    }
+    const [anchorEl, setAnchorEl] = useState<HTMLElement | undefined>(
+        undefined,
+    );
+
     const content = children[0];
     const hyphenated = toHyphenatedString(content);
     const link = router.asPath.split('#')[0] + '#' + hyphenated;
+
+    const handleHeaderClick = useCallback(
+        (event: React.MouseEvent<HTMLElement>) => {
+            if (typeof window === 'undefined') {
+                return;
+            }
+            const fullUrl = window.location.origin + link;
+            window.navigator.clipboard.writeText(fullUrl);
+            setAnchorEl(event.currentTarget);
+            setTimeout(() => setAnchorEl(undefined), 3000);
+        },
+        [link],
+    );
+
+    if (!children || !Array.isArray(children) || children.length > 1) {
+        return <h2>{children}</h2>;
+    }
 
     return (
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <Typography variant="h5" component="h2" id={hyphenated}>
                 {content}
             </Typography>
-            <Link href={link} passHref>
-                <IconButton>
-                    <LinkIcon fontSize="small" />
-                </IconButton>
-            </Link>
+            <IconButton onClick={handleHeaderClick}>
+                <LinkIcon fontSize="small" />
+            </IconButton>
+            <Popover
+                open={!!anchorEl}
+                anchorEl={anchorEl}
+                onClose={() => setAnchorEl(undefined)}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+            >
+                <Typography sx={{ p: 2 }}>Link copied to clipboard!</Typography>
+            </Popover>
         </Box>
     );
 };
